6d5dba9af10c27fee39e250a6b2e871aa6dd7628
Benoit Botton <benoit.botton@gmail.com>
2023-04-30 12:45:41 +0100

    static scene

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Tres from '@tresjs/core'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app=createApp(App)
+app.use(Tres)
+app.mount('#app')
