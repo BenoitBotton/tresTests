7e8fe61d7ec1f2418a0d1b4c017be8abe6f0c1e3
Benoit Botton <benoit.botton@gmail.com>
2023-09-16 11:59:24 +0100

    feat: add vector

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,31 +1,24 @@
 <script setup lang="ts">
-// import { useRenderLoop } from '@tresjs/core';
-import { OrbitControls } from '@tresjs/cientos';
-import {ref} from 'vue'
+// import { useRenderLoop } from '@tresjs/core'
+// import { TresCanvas } from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
+import { ref } from 'vue'
+import Vector from './Vector.vue'
 const boxRef = ref()
-// const {onLoop} =useRenderLoop()
 
-// onLoop(({delta, elapsed})=>{
-//   if(!boxRef.value) return
-//   boxRef.value.rotation.z+=delta
-//   boxRef.value.rotation.y=elapsed
-// })
 </script>
 
 <template>
-  <TresCanvas window-size>
-    <TresPerspectiveCamera :args="[75,1,0.1,1000]"
-    :position="[1,1,4]"
-    :look-at="[0,0,0]"
-    />
-    <OrbitControls/>
-      <TresScene>
-        <TresMesh ref="boxRef">
-          <TresBoxGeometry :args="[1,1,1]"/>
-          <TresMeshNormalMaterial/>
-        </TresMesh>
-        <TresAmbientLight :intensity="10" />
-      </TresScene>
-      <TresAxesHelper/>
+  <TresCanvas window-size preset="realistic">
+    <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]" :position="[5, 2, 5]" :look-at="[0, 0, 0]" />
+    <OrbitControls />
+    <TresScene>
+      <TresMesh ref="boxRef">
+        <Vector />
+      </TresMesh>
+      <TresAmbientLight :intensity="10" />
+    </TresScene>
+    <TresGridHelper />
+    <TresAxesHelper />
   </TresCanvas>
 </template>
---
+++ src/components/Vector.vue
@@ -0,0 +1,10 @@
+<template>
+    <Cone :args="[1, 2, 32]" :position="[0, 4, 0]" color="orange" />
+    <Box :args="[0.5, 3, 0.5]" :position="[0, 1.5, 0]" color="orange" />
+</template>
+
+<script setup lang="ts">
+import { Cone, Box } from '@tresjs/cientos'
+</script>
+
+<style scoped></style>
