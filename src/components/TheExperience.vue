b6e3df69c0850ff04a87c72d28d915811f03fb02
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-18 16:30:16 +0000

    remove animation and add lighting

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-// import { useRenderLoop } from '@tresjs/core'
-// import { TresCanvas } from '@tresjs/core'
+import { TresCanvas } from '@tresjs/core'
 import { OrbitControls } from '@tresjs/cientos'
-import { ref } from 'vue'
 import Vector from './Vector.vue'
-const boxRef = ref()
-
 </script>
 
 <template>
   <TresCanvas window-size preset="realistic">
     <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]" :position="[5, 2, 5]" :look-at="[0, 0, 0]" />
     <OrbitControls />
-    <TresScene>
-      <TresMesh ref="boxRef">
-        <Vector />
-      </TresMesh>
-      <TresAmbientLight :intensity="10" />
-    </TresScene>
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshLambertMaterial color="orange" />
+      <Vector />
+    </TresMesh>
+    <TresAmbientLight :intensity=".2" />
+    <TresDirectionalLight :intensity="1" :position="[500, 500, 0]" v-light-helper />
+    <TresDirectionalLight :intensity="0.5" :position="[-500, 500, 0]" v-light-helper />
     <TresGridHelper />
     <TresAxesHelper />
   </TresCanvas>
--- src/components/Vector.vue
+++ src/components/Vector.vue
@@ -1,10 +1,16 @@
 <template>
-    <Cone :args="[1, 2, 32]" :position="[0, 4, 0]" color="orange" />
-    <Box :args="[0.5, 3, 0.5]" :position="[0, 1.5, 0]" color="orange" />
+    <Cone :args="[1, 2, 32]" :position="[0, 4, 0]">
+        <TresMeshLambertMaterial :color="color" />
+    </Cone>
+    <Box :args="[0.5, 3, 0.5]" :position="[0, 1.5, 0]">
+        <TresMeshLambertMaterial :color="color" />
+    </Box>
 </template>
 
 <script setup lang="ts">
 import { Cone, Box } from '@tresjs/cientos'
+
+const color = 'red'
 </script>
 
 <style scoped></style>
