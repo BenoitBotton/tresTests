272206ff22627afa83bda7a54ba4085962408c0c
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-19 16:09:56 +0000

    wip: vector with cylinder

--- src/components/Vector.vue
+++ src/components/Vector.vue
@@ -2,13 +2,14 @@
     <Cone :args="[1, 2, 32]" :position="[0, 4, 0]">
         <TresMeshLambertMaterial :color="color" />
     </Cone>
-    <Box :args="[0.5, 3, 0.5]" :position="[0, 1.5, 0]">
+    <TresMesh :position="[0, 1.5, 0]">
+        <TresCylinderGeometry :args="[0.5, 0.5, 3]" />
         <TresMeshLambertMaterial :color="color" />
-    </Box>
+    </TresMesh>
 </template>
 
 <script setup lang="ts">
-import { Cone, Box } from '@tresjs/cientos'
+import { Cone } from '@tresjs/cientos'
 
 const color = 'red'
 </script>
