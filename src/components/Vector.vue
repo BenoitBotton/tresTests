31efdc732a69389c88e13fa41aec26732836d25c
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-22 11:45:53 +0000

    Vector is arrowhelper

--- src/components/Vector.vue
+++ src/components/Vector.vue
@@ -1,15 +1,9 @@
 <template>
-    <Cone :args="[1, 2, 32]" :position="[0, 4, 0]">
-        <TresMeshLambertMaterial :color="color" />
-    </Cone>
-    <TresMesh :position="[0, 1.5, 0]">
-        <TresCylinderGeometry :args="[0.5, 0.5, 3]" />
-        <TresMeshLambertMaterial :color="color" />
-    </TresMesh>
-</template>
+    <TresArrowHelper :args="[new THREE.Vector3(0, 1, 0), , 3, color]" />
+ </template>
 
 <script setup lang="ts">
-import { Cone } from '@tresjs/cientos'
+import * as THREE from 'three'
 
 const color = 'red'
 </script>
