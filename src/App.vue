d035b128a50cd559c2e15cc40da68129506fbe7f
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-22 11:46:34 +0000

    replace <main> with <suspense> at app level

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import TheExperience from './components/TheExperience.vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import TheExperience from './components/TheExperience.vue';
 </script>
 
 <template>
-  <main>
+  <Suspense>
     <TheExperience />
-  </main>
+  </Suspense>
 </template>
-
