c42800a565984c9d994da81848455065f7f00643
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-23 11:52:13 +0000

    wrap vector is TresGroup for v-if to work

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -4,11 +4,11 @@ import { OrbitControls } from '@tresjs/cientos'
 import Vector from './Vector.vue'
 import { ref } from 'vue'
 
-const exists = ref(true)
+const showVector = ref(true)
 </script>
 
 <template>
-  <input type="checkbox" v-model="exists" /> Show vector
+  <input type="checkbox" v-model="showVector" /> Show vector
   <TresCanvas style="height: 100vh" preset="realistic" v-bind="{ clearColor: '#4f4f4f' }">
     <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]" :position="[5, 2, 5]" :look-at="[0, 0, 0]" />
     <OrbitControls />
@@ -16,7 +16,9 @@ const exists = ref(true)
       <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
       <TresMeshLambertMaterial color="orange" />
     </TresMesh>
-    <Vector v-if="exists" rotation-x="1.5707"/>
+    <TresGroup v-if="showVector" rotation-x="-1.5707">
+      <Vector />
+    </TresGroup>
     <TresAmbientLight :intensity=".2" />
     <TresDirectionalLight :intensity="1" :position="[500, 500, 0]" />
     <TresDirectionalLight :intensity="0.5" :position="[-500, 500, 0]" />
