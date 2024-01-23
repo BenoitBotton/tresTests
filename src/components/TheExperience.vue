7efc6da498a39b394d862cec1bed02c0497afde1
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-23 12:01:26 +0000

    add toggle for donut

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -5,14 +5,16 @@ import Vector from './Vector.vue'
 import { ref } from 'vue'
 
 const showVector = ref(true)
+const showDonut = ref(false)
 </script>
 
 <template>
+  <input type="checkbox" v-model="showDonut" /> Show donut
   <input type="checkbox" v-model="showVector" /> Show vector
   <TresCanvas style="height: 100vh" preset="realistic" v-bind="{ clearColor: '#4f4f4f' }">
     <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]" :position="[5, 2, 5]" :look-at="[0, 0, 0]" />
     <OrbitControls />
-    <TresMesh>
+    <TresMesh v-if="showDonut">
       <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
       <TresMeshLambertMaterial color="orange" />
     </TresMesh>
