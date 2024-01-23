59ec6295f7859cf30da5eec1bd4ceacc882a4ffc
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-23 11:49:18 +0000

    Add text to vector

--- src/components/Vector.vue
+++ src/components/Vector.vue
@@ -1,10 +1,18 @@
 <template>
     <TresArrowHelper :args="[new THREE.Vector3(0, 1, 0), , 3, color]" />
- </template>
+    <Suspense>
+        <Text3D text="TresJS" :font="fontPath" :position-y="1.5">
+            <TresMeshLambertMaterial :color="color"/>
+        </Text3D>
+    </Suspense>
+</template>
 
 <script setup lang="ts">
 import * as THREE from 'three'
+import { Text3D } from '@tresjs/cientos'
 
+const fontPath =
+  'https://raw.githubusercontent.com/Tresjs/assets/main/fonts/FiraCodeRegular.json';
 const color = 'red'
 </script>
 
