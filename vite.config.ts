a1abb517d525cf8a11def764966b70f22ef44832
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-23 15:49:58 +0000

    add missing templateCompilerOptions to vit config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +9,7 @@ export default defineConfig({
       template: {
         compilerOptions: {
           isCustomElement: tag => tag.startsWith('Tres') && tag !== 'TresCanvas' || ['primitive'].includes(tag),
+          ...templateCompilerOptions,
         },
       },
     }
