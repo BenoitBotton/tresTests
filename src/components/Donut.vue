e53d3ab934415ea21d30d49b85eba56b5da94cf9
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-23 12:24:40 +0000

    create Donut component

---
+++ src/components/Donut.vue
@@ -0,0 +1,22 @@
+<template>
+    <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshLambertMaterial :color="color" />
+    </TresMesh>
+    <TresGroup v-if="showVector" rotation-x="-1.5707">
+        <Vector :txt="txt" :color="color" />
+    </TresGroup>
+</template>
+
+<script setup lang="ts">
+import Vector from './Vector.vue'
+
+const props = defineProps({
+    txt: String,
+    color: String,
+    showVector: Boolean,
+})
+
+</script>
+
+<style scoped></style>
--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,26 +1,18 @@
 <script setup lang="ts">
 import { TresCanvas } from '@tresjs/core'
 import { OrbitControls } from '@tresjs/cientos'
-import Vector from './Vector.vue'
 import { ref } from 'vue'
+import Donut from './Donut.vue'
 
 const showVector = ref(true)
-const showDonut = ref(false)
 </script>
 
 <template>
-  <input type="checkbox" v-model="showDonut" /> Show donut
   <input type="checkbox" v-model="showVector" /> Show vector
   <TresCanvas style="height: 100vh" preset="realistic" v-bind="{ clearColor: '#4f4f4f' }">
     <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]" :position="[5, 2, 5]" :look-at="[0, 0, 0]" />
     <OrbitControls />
-    <TresMesh v-if="showDonut">
-      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
-      <TresMeshLambertMaterial color="orange" />
-    </TresMesh>
-    <TresGroup v-if="showVector" rotation-x="-1.5707">
-      <Vector txt="Donut" color="green" />
-    </TresGroup>
+    <Donut :showVector="showVector" txt="Donut" color="orange" />
     <TresAmbientLight :intensity=".2" />
     <TresDirectionalLight :intensity="1" :position="[500, 500, 0]" />
     <TresDirectionalLight :intensity="0.5" :position="[-500, 500, 0]" />
