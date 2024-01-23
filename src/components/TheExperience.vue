088f1308d0a0470a649f112b270a1e8c14acd107
Benoit Botton <benoit.botton@zerdalab.com>
2024-01-23 12:54:23 +0000

    pass props as an object

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -5,6 +5,18 @@ import { ref } from 'vue'
 import Donut from './Donut.vue'
 
 const showVector = ref(true)
+
+const myDonut = {
+  radius: 2,
+  theta: 1,
+  height: 1,
+  x_rot: 0.5,
+  y_rot: 1.2,
+  z_rot: 1,
+  txt: 'Donut',
+  color: 'orange',
+}
+
 </script>
 
 <template>
@@ -12,7 +24,7 @@ const showVector = ref(true)
   <TresCanvas style="height: 100vh" preset="realistic" v-bind="{ clearColor: '#4f4f4f' }">
     <TresPerspectiveCamera :args="[75, 1, 0.1, 1000]" :position="[5, 2, 5]" :look-at="[0, 0, 0]" />
     <OrbitControls />
-    <Donut :radius="1" :showVector="showVector" txt="Donut" color="orange" />
+    <Donut v-bind="myDonut" :showVector="showVector"/>
     <TresAmbientLight :intensity=".2" />
     <TresDirectionalLight :intensity="1" :position="[500, 500, 0]" />
     <TresDirectionalLight :intensity="0.5" :position="[-500, 500, 0]" />
